30ee3a997816f7df74bafc16b97fcfe261764d56
Abduvoris <abikwizdev@gmail.com>
2024-01-14 23:11:16 +0500

    Initial Commit

---
+++ LoginView/LoginViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewApp.swift
+//  LoginView
+//
+//  Created by Abduvoris Abdullayev on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
